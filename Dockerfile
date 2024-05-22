bd568072461ba1314ad2d5db086f6694ac503454
Hu3rror <19755727+hu3rror@users.noreply.github.com>
2024-05-22 15:21:52 +0800

    feat(telegram-bot): move executable binary name from telegram_bot to memogram
    This commit moves the executable binary name from telegram_bot to memogram.
    
    This change is necessary because the binary name was previously incorrect and caused confusion. The new name is more accurate and will help to avoid confusion in the future.

--- Dockerfile
+++ Dockerfile
@@ -34,12 +34,10 @@ RUN if [ "$USE_MEMOGRAM" = "1" ]; then \
     apk add --no-cache gcompat procps && \
     wget https://github.com/usememos/telegram-integration/releases/download/v${MEMOGRAM_TAG}/memogram_v${MEMOGRAM_TAG}_linux_${TARGETARCH}.tar.gz && \
     tar -xvf memogram_v${MEMOGRAM_TAG}_linux_${TARGETARCH}.tar.gz && \
-    mkdir -p /usr/local/memos/telegram_bot && \
-    mv memogram /usr/local/memos/telegram_bot/ && \
-    mv /etc/memogram.env /usr/local/memos/telegram_bot/.env && \
+    mv /etc/memogram.env ./.env && \
     rm memogram_v${MEMOGRAM_TAG}_linux_${TARGETARCH}.tar.gz README.md && \
-    chown root:root ./telegram_bot/memogram && \
-    chmod +x ./telegram_bot/memogram; \
+    chown root:root ./memogram && \
+    chmod +x ./memogram; \
     fi
 
 # Define ENV
--- scripts/run.sh
+++ scripts/run.sh
@@ -11,7 +11,7 @@ use_litestream() {
 }
 
 use_memogram() {
-	[ -f "./telegram_bot/memogram" ] && [ -f "./telegram_bot/.env" ]
+	[ -x "./memogram" ] && [ -f "./.env" ]
 }
 
 # Main script logic
@@ -34,18 +34,18 @@ litestream replicate -exec "./memos" &
 if use_memogram; then
 	# Replace the MEMOGRAM_BOT_TOKEN placeholder in the .env file with the actual token value.
 	if [ -n "$MEMOGRAM_BOT_TOKEN" ]; then
-		sed -i 's/<MEMOGRAM_BOT_TOKEN>/'"$MEMOGRAM_BOT_TOKEN"'/g' ./telegram_bot/.env
+		sed -i 's/<MEMOGRAM_BOT_TOKEN>/'"$MEMOGRAM_BOT_TOKEN"'/g' ./.env
 	fi
 
 	# If the MEMOS_PORT environment variable is not set to the default value, replace it in the .env file.
 	if [ "$MEMOS_PORT" != "5230" ]; then
-		sed -i 's/5230/'"$MEMOS_PORT"'/g' ./telegram_bot/.env
+		sed -i 's/5230/'"$MEMOS_PORT"'/g' ./.env
 	fi
 
 	timeout=30
 	while [ $timeout -gt 0 ]; do
 		if pgrep -x "memos" >/dev/null && [ -f "$DB_PATH" ]; then
-			./telegram_bot/memogram
+			./memogram
 			break
 		else
 			echo "memos is not running, waiting for 5 seconds before retrying"
