7074517d4410a797b89b98e3279a2165dd4ead93
Hu3rror <19755727+hu3rror@users.noreply.github.com>
2024-05-22 12:34:30 +0800

    testing

--- scripts/run.sh
+++ scripts/run.sh
@@ -10,6 +10,10 @@ check_litestream_env() {
 		[ -n "$LITESTREAM_SECRET_ACCESS_KEY" ]
 }
 
+use_memogram() {
+	[ -f "./memogram" ] && [ -f "./.env" ]
+}
+
 # Main script logic
 if check_litestream_env; then
 	if [ -f "$DB_PATH" ]; then
@@ -25,4 +29,33 @@ fi
 
 # Start Litestream with the Memos service as the subprocess.
 echo "Starting litestream replicate with the Memos service as the subprocess."
-exec litestream replicate -exec "./memos"
+litestream replicate -exec "./memos" &
+
+if use_memogram; then
+	# Replace the MEMOGRAM_BOT_TOKEN placeholder in the .env file with the actual token value.
+	if [ -n "$MEMOGRAM_BOT_TOKEN" ]; then
+		sed -i 's/<MEMOGRAM_BOT_TOKEN>/'"$MEMOGRAM_BOT_TOKEN"'/g' ./.env
+	fi
+
+	# If the MEMOS_PORT environment variable is not set to the default value, replace it in the .env file.
+	if [ "$MEMOS_PORT" != "5230" ]; then
+		sed -i 's/5230/'"$MEMOS_PORT"'/g' ./.env
+	fi
+
+	timeout=30
+	while [ $timeout -gt 0 ]; do
+		if pgrep -x "memos" >/dev/null && [ -f "$DB_PATH" ]; then
+			./memogram
+			break
+		else
+			echo "memos is not running, waiting for 5 seconds before retrying"
+			sleep 5
+			timeout=$((timeout - 5))
+		fi
+	done
+
+	if [ $timeout -eq 0 ]; then
+		echo "over 30 seconds, memos is still not running, exiting"
+		exit 1
+	fi
+fi
