5447db7f3f0b5d5c2eda85806114ee057d58f045
Hu3rror <19755727+hu3rror@users.noreply.github.com>
2024-05-22 10:13:59 +0800

    feat(litestream): Adds support for litestream
    This commit adds support for litestream, a tool that can be used to replicate a database to a remote location. This allows for easier deployment of the Memos application and can improve performance.
    
    The following changes were made to support litestream:
    
    - Added a new script, `run-memos-with-litestream.sh`, which is used to start litestream and the Memos application.
    - Added a new environment variable, `LITESTREAM_REPLICA_BUCKET`, which is used to specify the name of the bucket where the database replica will be stored.
    - Added a new environment variable, `LITESTREAM_REPLICA_PATH`, which is used to specify the path to the database replica within the bucket.
    - Added a new environment variable, `LITESTREAM_REPLICA_ENDPOINT`, which is used to specify the endpoint of the bucket where the database replica will be stored.
    - Added a new environment variable, `LITESTREAM_ACCESS_KEY_ID`, which is used to specify the access key ID for the bucket where the database replica will be stored.
    - Added a new environment variable, `LITESTREAM_SECRET_ACCESS_KEY`, which is used to specify the secret access key for the bucket where the database replica will be stored.

--- scripts/run-memos-with-litestream.sh
+++ scripts/run-memos-with-litestream.sh
@@ -1,15 +1,33 @@
 #!/bin/sh
 set -e
 
+# Check if the environment variables required for Litestream are set.
+use_litestream() {
+	# Check if all the required environment variables are set.
+	[ -n "$LITESTREAM_REPLICA_BUCKET" ] && \
+	[ -n "$LITESTREAM_REPLICA_PATH" ] && \
+	[ -n "$LITESTREAM_REPLICA_ENDPOINT" ] && \
+	[ -n "$LITESTREAM_ACCESS_KEY_ID" ] && \
+	[ -n "$LITESTREAM_SECRET_ACCESS_KEY" ]
+}
+
 # Restore the database if it does not already exist.
-if [ -f "$DB_PATH" ]; then
-	echo "Database exists, skipping restore."
+if use_litestream; then
+    # Check if the database file exists.
+    if [ -f "$DB_PATH" ]; then
+    	# If the database exists, back it up to the backup directory.
+    	echo "An old local database was found and will be backed up to the $BACKUP_DIR directory."
+    	BACKUP_DIR="/var/opt/memos/.backup/$(date +'%Y-%m-%d_%H-%M-%S')"
+    	mkdir -p "$BACKUP_DIR"
+    	mv /var/opt/memos/memos_prod.db* "$BACKUP_DIR"
+	fi
 else
+	# If the database does not exist, attempt to restore it from a replica.
 	echo "No database found, attempt to restore from a replica."
 	litestream restore -if-replica-exists "$DB_PATH"
-	echo "Finished restoring the database."
-fi
+	echo "Database restore completed!"
 
-# Run litestream with your app as the subprocess.
-echo "Starting litestream & memos service."
-exec litestream replicate -exec "./memos"
+	# Run Litestream with the Memos service as the subprocess.
+	echo "Starting litestream & memos service."
+	exec litestream replicate -exec "./memos"
+fi
