16dac9c39721d00a5eb0a85770289c90bbb67db3
Hu3rror <19755727+hu3rror@users.noreply.github.com>
2024-05-22 10:13:11 +0800

    🔧 (scripts/run-memogram.sh): refactor script to handle environment variables more dynamically   🔧 (.env.example): update MEMOGRAM_SERVER_ADDR to use MEMOS_PORT for dynamic port configuration

--- scripts/run-memogram.sh
+++ scripts/run-memogram.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
 set -e
 
-# Replace MEMOGRAM_BOT_TOKEN in .env
-if [ -f "./memogram" ] && [ -f "./.env" ] && [ -n "$MEMOGRAM_BOT_TOKEN" ]; then
-	# Replace MEMOGRAM_BOT_TOKEN in .env
-	sed -i 's/<MEMOGRAM_BOT_TOKEN>/'"$MEMOGRAM_BOT_TOKEN"'/g' ./.env
+use_memogram() {
+	[ -f "./memogram" ] && \
+	[ -f "./.env" ]
+}
 
-	# Replace MEMOS_PORT in .env
+if use_memogram; then
+	# Replace the MEMOGRAM_BOT_TOKEN placeholder in the .env file with the actual token value.
+	if [ -n "$MEMOGRAM_BOT_TOKEN" ]; then
+		sed -i 's/<MEMOGRAM_BOT_TOKEN>/'"$MEMOGRAM_BOT_TOKEN"'/g' ./.env
+	fi
+
+	# If the MEMOS_PORT environment variable is not set to the default value, replace it in the .env file.
 	if [ "$MEMOS_PORT" != "5230" ]; then
 		sed -i 's/5230/'"$MEMOS_PORT"'/g' ./.env
 	fi
 
-    # Run memogram
-    echo "Starting memogram service."
-    exec ./memogram
+	# Start the memogram service.
+	echo "Starting memogram service."
+	exec ./memogram
 fi
