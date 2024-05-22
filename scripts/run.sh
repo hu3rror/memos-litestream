506b8d9d9de3f89da0a43a2be180d7ead8a742fc
Hu3rror <19755727+hu3rror@users.noreply.github.com>
2024-05-22 12:19:38 +0800

    refactor(db): Remove database backup & restore logic
    This commit removes the database backup and restore logic from the `run.sh` script.
    
    The database backup and restore logic was originally added as a way to protect against data loss in the event of a database corruption. However, this logic is no longer necessary because Litestream now provides built-in backup and restore functionality.
    
    Removing this logic simplifies the `run.sh` script and makes it more reliable.

--- scripts/run.sh
+++ scripts/run.sh
@@ -10,35 +10,16 @@ check_litestream_env() {
 		[ -n "$LITESTREAM_SECRET_ACCESS_KEY" ]
 }
 
-# Backup the database to a specified directory.
-backup_database() {
-	BACKUP_DIR="/var/opt/memos/.backup/$(date +'%Y-%m-%d_%H-%M-%S')"
-	echo "Backing up the database to $BACKUP_DIR directory."
-	mkdir -p "$BACKUP_DIR"
-	mv "$DB_PATH"* "$BACKUP_DIR"
-}
-
-# Restore the database from the replica or backup.
-restore_database() {
-	litestream restore -if-replica-exists "$DB_PATH"
-	if [ $? -ne 0 ]; then
-		echo "Database restore from replica failed!"
-		echo "Attempting to restore from backup..."
-		mv "$BACKUP_DIR/*" "/var/opt/memos/" && rm -rf "$BACKUP_DIR"
-		if ! [ -f "$DB_PATH" ]; then
-			echo "Database restore from backup failed! Exiting..."
-			exit 1
-		fi
-	fi
-}
-
 # Main script logic
 if check_litestream_env; then
 	if [ -f "$DB_PATH" ]; then
-		backup_database
-		restore_database
+		echo "Database exists, skipping restore."
+		echo "Tips: If you want to restore the version of database from S3/B2, please delete the $DB_PATH file and restart."
+		echo "Warning: Deleting the $DB_PATH file may cause data loss. Make sure to backup your database before deleting the $DB_PATH file."
 	else
-		restore_database
+		echo "No local database found, attempt to restore the latest version of database from S3/B2."
+		litestream restore -if-replica-exists "$DB_PATH"
+		echo "Database restored successfully!"
 	fi
 fi
 
