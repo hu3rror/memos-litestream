196c90613036b08b1c49b01a2a2420d7b6a82801
Hu3rror <19755727+hu3rror@users.noreply.github.com>
2024-05-22 11:52:17 +0800

    🔧 (run-memogram.sh): refactor script to remove duplicate check for DB_PATH existence and simplify logic   🚀 (run-memogram.sh): run memogram only when memos is running and DB_PATH exists to ensure correct execution

--- scripts/run-memogram.sh
+++ scripts/run-memogram.sh
@@ -16,22 +16,20 @@ if use_memogram; then
 		sed -i 's/5230/'"$MEMOS_PORT"'/g' ./.env
 	fi
 
-	if [ -f "$DB_PATH" ]; then
-		timeout=30
-		while [ $timeout -gt 0 ]; do
-			if pgrep -x "memos" >/dev/null; then
-				./memogram &
-				break
-			else
-				echo "memos is not running, waiting for 5 seconds before retrying"
-				sleep 5
-				timeout=$((timeout - 5))
-			fi
-		done
-
-		if [ $timeout -eq 0 ]; then
-			echo "over 30 seconds, memos is still not running, exiting"
-			exit 1
+	timeout=30
+	while [ $timeout -gt 0 ]; do
+		if pgrep -x "memos" >/dev/null && [ -f "$DB_PATH" ]; then
+			./memogram
+			break
+		else
+			echo "memos is not running, waiting for 5 seconds before retrying"
+			sleep 5
+			timeout=$((timeout - 5))
 		fi
+	done
+
+	if [ $timeout -eq 0 ]; then
+		echo "over 30 seconds, memos is still not running, exiting"
+		exit 1
 	fi
 fi
